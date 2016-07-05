4b2a511f3f1795f3eb3fcffeee1431ca093f55e9
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-07-05 14:38:03 -0700

    Done.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 // code solution here
+// function User (name, email){
+//   this.name = name;
+//   this.email = email;
+// }
+//
+// User.prototype.sayHello = function(){
+//   console.log("Hello, my name is "+ this.name);
+// }
+//
+// kevin = new User("kevin", "kevin@aol.com");
+//
+// kevin.sayHello();
+
+function President (name, politicalParty, yearsInOffice, homeState) {
+  this.name = name;
+  this.politicalParty = politicalParty;
+  this.yearsInOffice = yearsInOffice;
+  this.homeState = homeState;
+}
+
+President.prototype.veto = function() {
+  return ("NO!");
+}
+President.prototype.passBill = function() {
+  return ("You can do that!");
+}
+President.prototype.doCharity = function() {
+  return ("I like to help people.");
+}
+President.prototype.conductPressInterview = function() {
+  return ("I am proud to be an American.");
+}
+President.prototype.sayHi = function() {
+  return ("Hi, my name is " + this.name + ". " +  "I am from " + this.homeState +  ". " + "I represent the " + this.politicalParty +"s, " + "and was in office " + this.yearsInOffice + ".");
+}
